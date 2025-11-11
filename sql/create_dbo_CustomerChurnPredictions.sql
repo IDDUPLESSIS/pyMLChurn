80ed993b3aef150077aa79d854892f8da4c5fab0
David du Plessis <dave790822@gmail.com>
2025-11-11 08:52:53 -0500

    Add SQL scripts: table DDL and feature select.

---
+++ sql/create_dbo_CustomerChurnPredictions.sql
@@ -0,0 +1,30 @@
+﻿-- Creates the destination table used by pyMLChurn loads
+-- Default load mode in the app is REPLACE (overwrites table each run)
+
+IF OBJECT_ID(N'[dbo].[CustomerChurnPredictions]', N'U') IS NOT NULL
+BEGIN
+    DROP TABLE [dbo].[CustomerChurnPredictions];
+END
+GO
+
+CREATE TABLE [dbo].[CustomerChurnPredictions]
+(
+    [CustomerId]                     INT            NULL,
+    [SnapshotDate]                   DATE           NULL,
+    [DaysSinceLastPurchaseToday]     INT            NULL,
+    [ChurnedNowBusinessRule]         BIT            NULL,
+    [WhyBusinessRule]                NVARCHAR(MAX)  NULL,
+    [ChurnedWithin90DaysActual]      BIT            NULL,
+    [WhyTheyChurnedActual]           NVARCHAR(MAX)  NULL,
+    [PredictedToChurnNext90Days]     BIT            NULL,
+    [ChurnProbabilityPctNext90Days]  DECIMAL(5,2)   NULL,
+    [ChurnProbabilityNext90Days]     DECIMAL(9,6)   NULL,
+    [WhyAtRiskPredicted]             NVARCHAR(MAX)  NULL,
+    [CreatedOn]                      DATETIME       NULL
+);
+GO
+
+-- Helpful index for lookups by customer + snapshot
+CREATE NONCLUSTERED INDEX IX_CustomerChurnPredictions_Customer_Snapshot
+ON [dbo].[CustomerChurnPredictions] ([CustomerId], [SnapshotDate]);
+GO
---
+++ sql/select_customer_churn_features.sql
@@ -0,0 +1,34 @@
+﻿-- Mirrors the SELECT used by the app to fetch model features
+-- Use this for validation or ad-hoc analysis
+
+SELECT
+      [customer_id],
+      CONVERT(varchar(10), [t0], 23) AS [as_of_date], -- yyyy-mm-dd
+      [recency_days],
+      [median_gap_days],
+      [p90_gap_days],
+      [cv_gap],
+      [in_renewal_grace],
+      [rev_180d],
+      [rev_returns_90d],
+      [invoices_90d],
+      [credit_notes_90d],
+      [orders_pos_30d],
+      [orders_neg_30d],
+      [backorder_qty_30d],
+      [pct_change_3m],
+      [pct_change_6m],
+      [yoy_change_pct],
+      [credit_notes_prev_month],
+      [invoices_pos_prev_month],
+      [credit_notes_ma3],
+      [churned_hard90],
+      [threshold_days],
+      [is_maintenance_heavy],
+      [maint_cycle_days],
+      [severity_score],
+      [lateness_component],
+      [credits_component],
+      [trend_component],
+      [mitigator_component]
+FROM [SAP].[dbo].[CustomerChurnCadence_v1];
